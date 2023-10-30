b872e68364071935b7e263bbef2aa78ba127aa32
viniciusmarchioni <viniciusmarchionii@gmail.com>
2023-10-30 12:30:20 -0300

    commit

---
+++ app/src/main/java/com/example/charmelocal/CartActivity.kt
@@ -0,0 +1,11 @@
+package com.example.charmelocal
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CartActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cart)
+    }
+}
--- app/src/main/java/com/example/charmelocal/MainActivity.kt
+++ app/src/main/java/com/example/charmelocal/MainActivity.kt
@@ -1,34 +1,39 @@
 package com.example.charmelocal
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Layout
 import android.view.MenuItem
+import android.widget.ImageButton
 import androidx.fragment.app.Fragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
-
     //Fragmentos de navegação
     private lateinit var homeFragment: homeFragment
     private lateinit var searchFragment: searchFragment
     private lateinit var accountFragment: accountFragment
-
     private lateinit var bottomNavigationView: BottomNavigationView
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         //Instanciando fragmentos
         homeFragment = homeFragment()
         searchFragment = searchFragment()
         accountFragment = accountFragment()
-
+        val Cartbutton = findViewById<ImageButton>(R.id.CartButtom)
         bottomNavigationView = findViewById(R.id.bottom_navigation)
         bottomNavigationView.setOnNavigationItemSelectedListener(this)
 
-        setFragment(homeFragment)
 
+        Cartbutton.setOnClickListener{
+            goToSecoundLayout()
+        }
+
+        setFragment(homeFragment)
     }
 
     //função para chamar fragmentos
@@ -55,6 +60,11 @@ class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemS
             }
         }
         return true
+    }
+
+    private fun goToSecoundLayout(){
+        val secoundLayout = Intent(this,CartActivity::class.java)
+        startActivity(secoundLayout)
     }
 
 }
